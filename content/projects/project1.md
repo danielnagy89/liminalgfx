90bdfae5ae427675955601985620ebe2a336958b
Daniel <daniel.nagy.89@gmail.com>
2025-01-15 15:46:55 +0200

    Initial commit - 2025/01/15

---
+++ content/projects/project1.md
@@ -0,0 +1,5 @@
++++
+date = '2025-01-15T15:34:04+02:00'
+draft = true
+title = 'Project1'
++++
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.{html,js}", "./content/**/*.{md,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
