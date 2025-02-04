6812473a638d83241f06b30fdfbd64f2ad476fd1
Daniel <daniel.nagy.89@gmail.com>
2025-02-04 13:45:36 +0200

    wipeout HUGO iteration, 2025.02.04

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./layouts/**/*.{html,js}", "./content/**/*.{md,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
